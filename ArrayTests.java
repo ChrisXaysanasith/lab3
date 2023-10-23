3f9d40c7d081fe4fb1aaea7c4d4701d27211dc51
Chris Xaysanasith <cxaysanasith@ucsd.edu>
2023-10-23 15:22:14 -0700

    array changed

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,36 @@
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+	// @Test 
+	// public void testReverseInPlace() {
+  //   int[] input1 = { 3 };
+  //   ArrayExamples.reverseInPlace(input1);
+  //   assertArrayEquals(new int[]{ 3 }, input1);
+	// }
+
+
+  // @Test
+  // public void testReversed() {
+  //   int[] input1 = { };
+  //   assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  // }
+
+  @Test 
+	public void reverseTestEven() {
+    int[] input1 = {1, 2 ,3 ,4 ,5, 6};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, input1);
 	}
 
+  @Test 
+	public void reverseTestOdd() {
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, input1);
+	}
 
-  @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
 }
