5607ee4550cba3842e79fd200adc7c0a3231fd82
Chris Xaysanasith <cxaysanasith@ucsd.edu>
2023-11-05 22:03:35 -0800

    incorrect code

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,8 +1,19 @@
 
 
 public class ArrayExamples {
+  
+  
+  //  incorrect code
 
+  // static void reverseInPlace(int[] arr) {
+  //   for(int i = 0; i < arr.length; i += 1) {
+  //     arr[i] = arr[arr.length - i - 1];
+  //   }
+  // }
   // Changes the input array to be in reversed order
+
+  // corrected code
+
   static void reverseInPlace(int[] arr) {
     for(int i = 0; i < arr.length / 2; i += 1) {
       int temp = arr[i];
